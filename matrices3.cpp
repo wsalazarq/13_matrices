d507d243f63f85fd14b9c5b194e669e86da38169
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-15 10:50:04 -0500

    matrices_3: Verificacion si el elemento esta al borde de la matriz e imprimir la matriz generada

--- matrices3.cpp
+++ matrices3.cpp
@@ -9,5 +9,23 @@ int main() {
     int columnas = 3 * n;
     int matriz[filas][columnas];
 
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+       
+            if (i == 0 || i == filas - 1 || j == 0 || j == columnas - 1){
+                matriz[i][j] = 1;
+            }else{
+                matriz[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            cout<<matriz[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
