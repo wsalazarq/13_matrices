df485a8ca393d2cb33b5de7fd5f57d1de99bafe6
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-15 10:19:11 -0500

    Agregar matrices_1: Implementar la lectura de datos de la matriz

---
+++ matrices1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Ingrese el valor de n: ";cin>>n;
+    int matriz[n][n];
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout<<"Ingrese el valor de la posicion ["<<i+1<<"]"<<"["<<j+1<<"]";cin>>matriz[i][j];
+        }
+    }
+    
+    return 0;
+}
