4e64128d1ca5884b072a4d54c377332c1deff513
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-15 10:43:58 -0500

    Agregar matrices_3: Lectura del tamaño de la matriz

---
+++ matrices3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Ingrese el tamaño de la matriz: ";cin >> n;
+
+    int filas = n;
+    int columnas = 3 * n;
+    int matriz[filas][columnas];
+
+    return 0;
+}
