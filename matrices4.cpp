050ae4ec0a15a1684b78284312f2e7302a023790
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-20 16:07:22 -0500

    matrices_4: Imprimir los resultados

--- matrices4.cpp
+++ matrices4.cpp
@@ -12,7 +12,7 @@ int main() {
         }
     }
 
-    int totalMes = 0;
+    float totalMes = 0;
     for (int semana = 0; semana < 4; ++semana) {
         for (int dia = 0; dia < 7; ++dia) {
             totalSemanas[semana] += ventas[dia][semana];
@@ -20,6 +20,26 @@ int main() {
 
         totalMes += totalSemanas[semana];
     }
-    
+
+    cout<<"Ventas de cuatro semanas"<<endl;
+    cout<<"----------------------------------"<<endl;
+    cout << "     SEM1  SEM2  SEM3  SEM4"<<endl;
+
+    for (int dia = 0; dia < 7; ++dia) {
+        cout<<"("<<dias[dia]<<")";
+        for (int semana = 0; semana < 4; ++semana) {
+            cout<<"   "<<ventas[dia][semana];
+        }
+        cout << endl;
+    }
+
+    cout<<"----------------------------------"<<endl;
+    for (int semana = 0; semana < 4; ++semana) {
+        cout<<totalSemanas[semana]<<"  ";
+    }
+    cout<<endl;
+    cout<<"----------------------------------"<<endl;
+    cout<<"Venta total del mes: "<<totalMes<<endl;
+
     return 0;
 }
